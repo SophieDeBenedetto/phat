3b805c01fe71ea5ccf4222bb420034b88224143e
sophiedebenedetto <sophie.debenedetto@gmail.com>
2019-08-28 10:49:15 -0600

    refactor with JS hooks

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,41 +3,21 @@
 // its own CSS file.
 import css from "../css/app.css"
 
-// webpack automatically bundles all modules in your
-// entry points. Those entry points can be configured
-// in "webpack.config.js".
-//
-// Import dependencies
-//
 import "phoenix_html"
 
 import LiveSocket from "phoenix_live_view"
 
-let liveSocket = new LiveSocket("/live")
-liveSocket.connect()
-
-// Select the node that will be observed for mutations
 const targetNode = document.getElementsByClassName("messages")[0]
-
 document.addEventListener("DOMContentLoaded", function() {
   targetNode.scrollTop = targetNode.scrollHeight
 });
-// Options for the observer (which mutations to observe)
-let config = { attributes: true, childList: true, subtree: true };
-// Callback function to execute when mutations are observed
-var callback = function(mutationsList, observer) {
-  for(var mutation of mutationsList) {
-    if (mutation.type == 'childList') {
-      targetNode.scrollTop = targetNode.scrollHeight
-    }
-  }
-};
-// Create an observer instance linked to the callback function
-var observer = new MutationObserver(callback);
-// Start observing the target node for configured mutations
-observer.observe(targetNode, config);
 
-// Import local files
-//
-// Local files can be imported directly using relative paths, for example:
-// import socket from "./socket"
+let Hooks = {}
+Hooks.NewChatMessage = {
+  updated(){
+    this.el.scrollTop = this.el.scrollHeight
+  }
+}
+
+let liveSocket = new LiveSocket("/live", {hooks: Hooks})
+liveSocket.connect()
